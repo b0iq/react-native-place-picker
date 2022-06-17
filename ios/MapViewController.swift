027175655e230450fafd2e1c031ea68d974a26ef
b0iq <me@b0iq.com>
2022-06-18 03:10:21 +0400

    Day 01 Setup iOS

---
+++ ios/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  react-native-place-picker
+//
+//  Created by b0iq on 17/06/2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
