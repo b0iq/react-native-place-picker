60583096d72c8230c060a923c531ce47b8c0e3cc
b0iq <contact@b0iq.com>
2023-09-01 20:35:55 +0400

    iOS: UISearchBar cancel button (#11)
    * feat: search improvements
    
    * chore: release 2.7.0

---
+++ ios/Helpers/CustomSearchComplation.swift
@@ -0,0 +1,13 @@
+//
+//  CustomSearchComplation.swift
+//  react-native-place-picker
+//
+//  Created by b0iq on 01/09/2023.
+//
+
+import Foundation
+
+struct CustomSearchCompletion {
+    var attrTitle: NSAttributedString?
+    var attrSubtitle: NSAttributedString?
+}
---
+++ ios/Helpers/DropdownTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  DropDownCell.swift
+//  react-native-place-picker
+//
+//  Created by b0iq on 01/09/2023.
+//
+
+import UIKit
+
+class DropdownTableViewCell: UITableViewCell {
+
+    static let reuseIdentifier = "DropdownTableViewCell"
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        contentView.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    func set(_ text: String) {
+        label.text = text
+    }
+ 
+}
